7d325a3eb2960d95d2acc640885217072928f1cf
kingboseong <rlaqhtjd8001@naver.com>
2024-02-22 12:51:20 +0900

    js 실습

--- 03/03.js
+++ 03/03.js
@@ -1,1 +1,12 @@
-alert("안녕하세요")
+// alert("안녕하세요")
+
+function handleClick(n){
+    // 메세지 영역 가져오기1
+    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+
+    // 메세지 영역 가져오기2
+    // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+
+    // 메세지 영역 가져오기3
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
+}

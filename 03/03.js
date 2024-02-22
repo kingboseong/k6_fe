78a5fed9bc8959a13233313e70843fd3197604bd
kingboseong <rlaqhtjd8001@naver.com>
2024-02-22 13:10:12 +0900

    js 실습

--- 03/03.js
+++ 03/03.js
@@ -4,9 +4,14 @@
     함수 작성 2 : 화살표 함수
     ret = 변수
     const = 상수
- */ function handleClick(n) {
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
-}
+ */ 
+const handleClick = (n) => {
+    let msg ;
+    if ( n == 1 ) msg = "<h2>안녕하세요.</h2>";
+    else msg = "<h2>안녕히 가세요</h2>";
+    document.querySelector("#msgArea").innerHTML = msg;
+    }
+
 
 
 /* 

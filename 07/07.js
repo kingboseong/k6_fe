304cf8df2153c53f76b10fbfd45e9f552f8f2dcc
kingboseong <rlaqhtjd8001@naver.com>
2024-02-26 17:59:40 +0900

    02_26주석포함

--- 07/07.js
+++ 07/07.js
@@ -1,7 +1,7 @@
 const handleChange = (s1, s2, lb1, lb2, t1, t2) => {
     if (s1.value === "℃") {
-        s2.value = "Ｆ"
-        lb1.innerHTML = "℃"
+        s2.value = "Ｆ"  //input type(form 태그)를 가지고 올 떄
+        lb1.innerHTML = "℃" // 일반 태그를 가지고 올 때
         lb2.innerHTML = "Ｆ"
     } else {
         s2.value = "℃"
@@ -14,7 +14,11 @@ const handleChange = (s1, s2, lb1, lb2, t1, t2) => {
     t2.value = "";
 }
 
+
+//Domtree 완성 후 작성
 document.addEventListener("DOMContentLoaded", () => {
+    //html(select)에서 찝어내고 싶은 것들 찝어냄
+    //querySelector 함수명, 돔트리에 있는 select를 찾아줌.
     const sel1 = document.querySelector("#sel1")
     const sel2 = document.querySelector("#sel2")
 
@@ -23,6 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const labels = document.querySelectorAll("label")
 
+
+    //select박스에 이벤트를 발생하도록 만듦(addeventListener)
     txt1.addEventListener("input", () => {
         console.log(txt1.value)
         if (sel1.value === "℃") {
@@ -34,7 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         }
     })
-
 
     sel1.addEventListener("change", () => {
         handleChange(sel1, sel2, labels[0], labels[1], txt1, txt2)

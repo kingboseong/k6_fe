765fdb2c97cd6dd9af933cc5d241111c5dac5fa1
kingboseong <rlaqhtjd8001@naver.com>
2024-02-26 17:39:41 +0900

    02_26

--- 07/07.js
+++ 07/07.js
@@ -1,43 +1,47 @@
-const handleChange =(s1, s2, lb1, lb2) => {
-    if(s1.value === "℃") {
-        s2.value ="Ｆ" 
-        lb1.innerHTML = "℃" 
-        lb2.innerHTML = "Ｆ" 
-    }else{
+const handleChange = (s1, s2, lb1, lb2, t1, t2) => {
+    if (s1.value === "℃") {
+        s2.value = "Ｆ"
+        lb1.innerHTML = "℃"
+        lb2.innerHTML = "Ｆ"
+    } else {
         s2.value = "℃"
         lb1.innerHTML = "Ｆ"
-        lb2.innerHTML = "℃"  
+        lb2.innerHTML = "℃"
     }
+
+    t1.value = "";
+    t1.focus();
+    t2.value = "";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const sel1 =document.querySelector("#sel1")
-    const sel2 =document.querySelector("#sel2")
+    const sel1 = document.querySelector("#sel1")
+    const sel2 = document.querySelector("#sel2")
 
-    const txt1 =document.querySelector("#txt1")
-    const txt2 =document.querySelector("#txt2")
+    const txt1 = document.querySelector("#txt1")
+    const txt2 = document.querySelector("#txt2")
 
     const labels = document.querySelectorAll("label")
-    
-    txt1.addEventListener("change", () => {
+
+    txt1.addEventListener("input", () => {
         console.log(txt1.value)
-        if(sel1.value === "℃" ){
+        if (sel1.value === "℃") {
             //섭씨온도를 화씨온도로
-            txt2.value = (txt1.value * (9/5)) + 32;
-        }else{
+            txt2.value = (parseFloat(txt1.value) * (9 / 5)) + 32;
+        } else {
             //화씨온도를 섭씨온도로
-            txt2.value = (txt1.value - 32) * (5/9);
+            txt2.value = (parseFloat(txt1.value) - 32) * (5 / 9);
 
         }
     })
 
 
-    sel1.addEventListener("change", ()=>{
-        handleChange(sel1, sel2, labels[0], labels[1])
+    sel1.addEventListener("change", () => {
+        handleChange(sel1, sel2, labels[0], labels[1], txt1, txt2)
     })
     //form의 값은 .value로 가지고 온다.
-    sel1.addEventListener("change", ()=>{
-        handleChange(sel2, sel1, labels[1], labels[0])
+    sel1.addEventListener("change", () => {
+        handleChange(sel2, sel1, labels[1], labels[0], txt1, txt2)
     })
 
 })
